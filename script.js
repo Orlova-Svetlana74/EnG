80a45edb7953e781d24889fcd21e9e5eb26ef17f
Viktor <Blade-85@inbox.ru>
2025-03-03 16:29:08 +0300

    изменение текста ,картинок,ссылки

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var modal = document.getElementById('modal');
-    var aboutLink = document.getElementById('about-link');
-    var closeBtn = document.getElementsByClassName('close')[0];
+document.addEventListener("DOMContentLoaded", function () {
+  const modal = document.getElementById("modal");
+  const aboutLink = document.getElementById("about-link");
+  const closeBtn = document.getElementsByClassName("close")[0];
 
-    aboutLink.onclick = function(event) {
-        event.preventDefault();
-        modal.style.display = 'block';
-    }
+  aboutLink.onclick = function (event) {
+    event.preventDefault();
+    modal.style.display = "block";
+  };
 
-    closeBtn.onclick = function() {
-        modal.style.display = 'none';
-    }
+  closeBtn.onclick = function () {
+    modal.style.display = "none";
+  };
 
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = 'none';
-        }
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
     }
+  };
 
-    var form = document.getElementById('feedback-form');
-    form.onsubmit = function(event) {
-        event.preventDefault();
-        console.log('Форма отправлена');
-        modal.style.display = 'none';
-    }
+  var form = document.getElementById("feedback-form");
+  form.onsubmit = function (event) {
+    event.preventDefault();
+    console.log("Форма отправлена");
+    modal.style.display = "none";
+  };
 });
