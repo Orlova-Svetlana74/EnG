5d75557fd6ebb32b1c67058ec4d9a3cbc061179f
Svetlana Orlova <sorlova#mail.ru>
2025-08-10 00:22:36 +0300

    исправили index.ts

--- img/index.ts
+++ img/index.ts
@@ -1,35 +1,38 @@
-<<<<<<< HEAD
-import project1Img from './project_1.png';
-import mainShape from './main-shape.png';
-import footerShape from './footer-shape.png';
-import Frame from './Frame.svg';
-import logo from './logo.svg';
-import Shape_left_hi from './Shape_left_hi.svg';
-import Shape_left_down from './Shape_left_down.svg';
-import Shape_right_hi from './Shape_right_hi.svg';
-import Shape_right_down from './Shape_right_down.svg';
-
-
-
-import icon1 from './icon1.svg';
-import icon2 from './icon2.svg';
-import icon3 from './icon3.svg';
-import icon4 from './icon4.svg';
-import senior_back from './senior_back.svg';
-import senior_frontend from './senior_frontend.svg';
-import tg_invert from './tg_invert.svg';
-import senior_disagn from './senior_disagn.svg';
-import middle_back from './middle_back.svg';
-import middle_backend from './middle_backend.svg';
-import middle_front from './middle_front.svg';
-import junior_front from './junior_front.svg';
-import middle_disign from './middle_disign.svg';
-import project_manager from './middle_disign.svg';
+// import project1Img from "./project_1.png";
+import mainShape from "./main-shape.png";
+import footerShape from "./footer-shape.png";
+import Frame from "./Frame.svg";
+import logo from "./logo.svg";
+import Shape_left_hi from "./Shape_left_hi.svg";
+import Shape_left_down from "./Shape_left_down.svg";
+import Shape_right_hi from "./Shape_right_hi.svg";
+import Shape_right_down from "./Shape_right_down.svg";
+import icon1 from "./icon1.svg";
+import icon2 from "./icon2.svg";
+import icon3 from "./icon3.svg";
+import icon4 from "./icon4.svg";
+import senior_back from "./senior_back.svg";
+import senior_frontend from "./senior_frontend.svg";
+import tg_invert from "./tg_invert.svg";
+import senior_disagn from "./senior_disagn.svg";
+import middle_back from "./middle_back.svg";
+import middle_backend from "./middle_backend.svg";
+import middle_front from "./middle_front.svg";
+import junior_front from "./junior_front.svg";
+import middle_disign from "./middle_disign.svg";
+import project_manager from "./middle_disign.svg";
+import projectKitchenImg from "./project-kitchen.png";
+import projectRukalineImg from "./project-runkaline.png";
+import projectFoodImg from "./project-food.png";
+import projectMandarinaImg from "./project-kitchen.png";
+import descriptionBigShape from "./shape-description-big.png";
+import descriptionSmallShape from "./shape-description-small.png";
+import offerShape from "./shape-offer.png";
+import projectsShape from "./shape-projects.png";
+import skillsShape from "./shape-projects.png";
 
 export {
-  project1Img,
-  mainShape,
-  footerShape,
+  // project1Img,
   Frame,
   logo,
   Shape_left_hi,
@@ -50,20 +53,6 @@ export {
   junior_front,
   middle_disign,
   project_manager,
-=======
-import projectKitchenImg from "./project-kitchen.png";
-import projectRukalineImg from "./project-runkaline.png";
-import projectFoodImg from "./project-food.png";
-import projectMandarinaImg from "./project-kitchen.png";
-import mainShape from "./main-shape.png";
-import footerShape from "./footer-shape.png";
-import descriptionBigShape from "./shape-description-big.png";
-import descriptionSmallShape from "./shape-description-small.png";
-import offerShape from "./shape-offer.png";
-import projectsShape from "./shape-projects.png";
-import skillsShape from "./shape-projects.png";
-
-export {
   projectKitchenImg,
   projectRukalineImg,
   projectFoodImg,
@@ -75,5 +64,4 @@ export {
   offerShape,
   projectsShape,
   skillsShape,
->>>>>>> 850d5f6bfffe7ddf54be87170c713af9f9099f30
 };
