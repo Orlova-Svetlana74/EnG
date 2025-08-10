2de8b6ddce0bbc43885d417555e5a70f53c09300
ratnov2 <anton.ratnov@yandex.ru>
2025-08-10 12:51:40 +0300

    feat: add prettier

--- components/ui/Button/Button.tsx
+++ components/ui/Button/Button.tsx
@@ -1,30 +1,30 @@
-import styles from "./Button.module.scss";
+import styles from './Button.module.scss'
 interface IButton extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  children: React.ReactNode;
-  onClick?: () => void;
-  disabled?: boolean;
-  loading?: boolean;
+    children: React.ReactNode
+    onClick?: () => void
+    disabled?: boolean
+    loading?: boolean
 }
 
 export const Button = ({
-  children,
-  onClick,
-  disabled,
-  loading,
-  className,
-  ...rest
+    children,
+    onClick,
+    disabled,
+    loading,
+    className,
+    ...rest
 }: IButton) => {
-  return (
-    <button
-      className={`${styles.button} ${className} ${
-        loading ? styles.loading : ""
-      }`}
-      disabled={disabled}
-      onClick={onClick}
-      {...rest}
-    >
-      {loading && <span className={styles.spinner} />}
-      {children}
-    </button>
-  );
-};
+    return (
+        <button
+            className={`${styles.button} ${className} ${
+                loading ? styles.loading : ''
+            }`}
+            disabled={disabled}
+            onClick={onClick}
+            {...rest}
+        >
+            {loading && <span className={styles.spinner} />}
+            {children}
+        </button>
+    )
+}
--- img/index.ts
+++ img/index.ts
@@ -1,25 +1,25 @@
-import projectKitchenImg from "./project-kitchen.png";
-import projectRukalineImg from "./project-runkaline.png";
-import projectFoodImg from "./project-food.png";
-import projectMandarinaImg from "./project-kitchen.png";
-import mainShape from "./main-shape.png";
-import footerShape from "./footer-shape.png";
-import descriptionBigShape from "./shape-description-big.png";
-import descriptionSmallShape from "./shape-description-small.png";
-import offerShape from "./shape-offer.png";
-import projectsShape from "./shape-projects.png";
-import skillsShape from "./shape-projects.png";
+import projectKitchenImg from './project-kitchen.png'
+import projectRukalineImg from './project-runkaline.png'
+import projectFoodImg from './project-food.png'
+import projectMandarinaImg from './project-kitchen.png'
+import mainShape from './main-shape.png'
+import footerShape from './footer-shape.png'
+import descriptionBigShape from './shape-description-big.png'
+import descriptionSmallShape from './shape-description-small.png'
+import offerShape from './shape-offer.png'
+import projectsShape from './shape-projects.png'
+import skillsShape from './shape-projects.png'
 
 export {
-  projectKitchenImg,
-  projectRukalineImg,
-  projectFoodImg,
-  projectMandarinaImg,
-  mainShape,
-  footerShape,
-  descriptionBigShape,
-  descriptionSmallShape,
-  offerShape,
-  projectsShape,
-  skillsShape,
-};
+    projectKitchenImg,
+    projectRukalineImg,
+    projectFoodImg,
+    projectMandarinaImg,
+    mainShape,
+    footerShape,
+    descriptionBigShape,
+    descriptionSmallShape,
+    offerShape,
+    projectsShape,
+    skillsShape,
+}
