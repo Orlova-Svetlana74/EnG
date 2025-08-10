ef75d240d4dd49e5ec632aa3791d181148d86110
Svetlana Orlova <sorlova#mail.ru>
2025-08-10 21:34:51 +0300

    исправили index.ts

--- img/index.ts
+++ img/index.ts
@@ -1,72 +1,3 @@
-<<<<<<< HEAD
-// import project1Img from "./project_1.png";
-import mainShape from "./main-shape.png";
-import footerShape from "./footer-shape.png";
-import Frame from "./Frame.svg";
-import logo from "./logo.svg";
-import Shape_left_hi from "./Shape_left_hi.svg";
-import Shape_left_down from "./Shape_left_down.svg";
-import Shape_right_hi from "./Shape_right_hi.svg";
-import Shape_right_down from "./Shape_right_down.svg";
-import icon1 from "./icon1.svg";
-import icon2 from "./icon2.svg";
-import icon3 from "./icon3.svg";
-import icon4 from "./icon4.svg";
-import senior_back from "./senior_back.svg";
-import senior_frontend from "./senior_frontend.svg";
-import tg_invert from "./tg_invert.svg";
-import senior_disagn from "./senior_disagn.svg";
-import middle_back from "./middle_back.svg";
-import middle_backend from "./middle_backend.svg";
-import middle_front from "./middle_front.svg";
-import junior_front from "./junior_front.svg";
-import middle_disign from "./middle_disign.svg";
-import project_manager from "./middle_disign.svg";
-import projectKitchenImg from "./project-kitchen.png";
-import projectRukalineImg from "./project-runkaline.png";
-import projectFoodImg from "./project-food.png";
-import projectMandarinaImg from "./project-kitchen.png";
-import descriptionBigShape from "./shape-description-big.png";
-import descriptionSmallShape from "./shape-description-small.png";
-import offerShape from "./shape-offer.png";
-import projectsShape from "./shape-projects.png";
-import skillsShape from "./shape-projects.png";
-
-export {
-  // project1Img,
-  Frame,
-  logo,
-  Shape_left_hi,
-  Shape_left_down,
-  Shape_right_down,
-  Shape_right_hi,
-  icon1,
-  icon2,
-  icon3,
-  icon4,
-  senior_back,
-  senior_frontend,
-  tg_invert,
-  senior_disagn,
-  middle_back,
-  middle_backend,
-  middle_front,
-  junior_front,
-  middle_disign,
-  project_manager,
-  projectKitchenImg,
-  projectRukalineImg,
-  projectFoodImg,
-  projectMandarinaImg,
-  mainShape,
-  footerShape,
-  descriptionBigShape,
-  descriptionSmallShape,
-  offerShape,
-  projectsShape,
-  skillsShape,
-};
-=======
 import projectKitchenImg from './project-kitchen.png'
 import projectRukalineImg from './project-runkaline.png'
 import projectFoodImg from './project-food.png'
@@ -92,4 +23,3 @@ export {
     projectsShape,
     skillsShape,
 }
->>>>>>> 2de8b6ddce0bbc43885d417555e5a70f53c09300
