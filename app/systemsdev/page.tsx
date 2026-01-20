b4fbb963f6f4f5d8f0b84183df8e1ede062a4c72
Svetlana Orlova <sorlova#mail.ru>
2026-01-21 02:53:09 +0300

    add page testing fix

--- app/systemsdev/page.tsx
+++ app/systemsdev/page.tsx
@@ -11,13 +11,11 @@ import LMSComponentSVG from '@/svg/LMSComponentSVG'
 import ERPComponentSVG from '@/svg/ERPComponentSVG'
 import { Footer } from '@/components/footer/Footer'
 import Plus from '@/svg/PlusSVG'
-
 import { faqDatasystems } from '@/data/datasystemsdev'
 import Blockmodal from '@/components/ui/blockModal/blockmodal'
 import { Frametwo } from '@/public/img'
 
-import {
-    descriptionSmallShape,
+import {    
     descriptionBigShape,
     offerShape,
     projectsShape,
@@ -63,8 +61,7 @@ export default function Systemsdev() {
                     <br /> системы CRM, CMS, ERP, LMS
                 </h1>
 
-                <section className={styles.page__developsection}>
-                    {/* <div className={styles.page__developtitle}></div> */}
+                <section className={styles.page__developsection}>                    
                     <div>
                         <p className={styles.page__titletext}>
                             Мы создаём и внедряем программное обеспечение,
