2c21bfe628481fa4b1e1c6724d204fa9e18c6db0
Svetlana Orlova <sorlova#mail.ru>
2026-05-14 02:36:31 +0300

    deploy: update with correct paths

---
+++ img/index.ts
@@ -0,0 +1,137 @@
+import projectKitchenImg from './project-kitchen.png'
+import projectRukalineImg from './project-runkaline.png'
+import projectFoodImg from './project-food.png'
+import projectMandarinaImg from './project-kitchen.png'
+import mainShape from './main-shape.png'
+import footerShape from './footer-shape.png'
+import descriptionBigShape from './shape-description-big.png'
+import descriptionSmallShape from './shape-description-small.png'
+import offerShape from './shape-offer.png'
+import projectsShape from './shape-projects.png'
+import skillsShape from './shape-skills.png'
+import seniorBack from './senior_back.svg'
+import seniorFrontend from './senior_frontend.svg'
+import middleBack from './middle_back.svg'
+import seniorDisign from './senior_disagn.svg'
+import middleBackend from './middle_backend.svg'
+import middleFront from './middle_front.svg'
+import juniorFront from './junior_front.svg'
+import middleDisign from './middle_disign.svg'
+import projectManager from './project_manager.svg'
+import icon1 from './icon1.svg'
+import icon2 from './icon2.svg'
+import icon3 from './icon3.svg'
+import icon4 from './icon4.svg'
+import frame from './framefr.svg'
+import Frametwo from './Frametwo.svg'
+import icongear from './icongear.svg'
+import settingsalert from './settings_alert.svg'
+import webhook from './webhook.svg'
+import workspacepremium from './workspace_premium.svg'
+import sync from './sync.svg'
+import group from './group.svg'
+import handyman from './handyman.svg'
+import paymentarrowdown from './payment_arrow_down.svg'
+import shoppingcart from './shoppingcart.svg'
+import creditcard from './creditcard.svg'
+import game from './game.svg'
+import sms from './sms.svg'
+import joystick from './joystick.svg'
+import health_and_safety from './health_and_safety.svg'
+import school from './school.svg'
+import currency_exchange from './currency_exchange.svg'
+import checkcircle from './check_circle.svg'
+import iconapplication from './iconapplication.svg'
+import icondatabase from './icondatabase.svg'
+import icondevices from './icondevices.svg'
+import icon_display from './icon_display.svg'
+import icontactic from './icontactic.svg'
+import cognition from './cognition.svg'
+import folder_open from './folder_open.svg'
+import grain from './grain.svg'
+import graph_5 from './graph_5.svg'
+import lock from './lock.svg'
+import widgets from './widgets.svg'
+import AI1 from './AI1.svg'
+import AI2 from './AI2.svg'
+import AI3 from './AI3.svg'
+import AI4 from './AI4.svg'
+import AI5 from './AI5.svg'
+import AI6 from './AI6.svg'
+import AI7 from './AI7.svg'
+import AI8 from './AI8.svg'
+import ErrorVector from './ErrorVector.svg'
+import ErrorVector2 from './ErrorVector2.svg'
+import ErrorVector3 from './ErrorVector3.svg'
+import check_box from './check_box.svg'
+import Shape_ from './Shape_.svg'
+
+export {
+    projectKitchenImg,
+    projectRukalineImg,
+    projectFoodImg,
+    projectMandarinaImg,
+    mainShape,
+    footerShape,
+    descriptionBigShape,
+    descriptionSmallShape,
+    offerShape,
+    projectsShape,
+    skillsShape,
+    seniorBack,
+    seniorFrontend,
+    seniorDisign,
+    middleBack,
+    middleBackend,
+    middleFront,
+    juniorFront,
+    middleDisign,
+    projectManager,
+    icon1,
+    icon2,
+    icon3,
+    icon4,
+    frame,
+    Frametwo,
+    icongear,
+    settingsalert,
+    webhook,
+    workspacepremium,
+    sync,
+    group,
+    handyman,
+    paymentarrowdown,
+    shoppingcart,
+    creditcard,
+    game,
+    sms,
+    joystick,
+    health_and_safety,
+    school,
+    currency_exchange,
+    checkcircle,
+    iconapplication,
+    icondatabase,
+    icondevices,
+    icon_display,
+    icontactic,
+    cognition,
+    folder_open,
+    grain,
+    graph_5,
+    lock,
+    widgets,
+    AI1,
+    AI2,
+    AI3,
+    AI4,
+    AI5,
+    AI6,
+    AI7,
+    AI8,
+    check_box,
+    ErrorVector,
+    ErrorVector2,
+    ErrorVector3,
+    Shape_  
+}
