dc89bc8b9cc9f134b06a3d0be7b8549e1f6a57a1
Svetlana Orlova <sorlova#mail.ru>
2026-05-14 02:11:50 +0300

    temporarily disable dynamic [id] page for static export

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from 'next'
-
+module.exports = {
+    output: 'export', // Эта строчка включает экспорт в статику
+    images: {
+        unoptimized: true, // Отключает оптимизацию изображений (иначе ошибка)
+    },
+}
 const nextConfig: NextConfig = {
     /* config options here */
 }
